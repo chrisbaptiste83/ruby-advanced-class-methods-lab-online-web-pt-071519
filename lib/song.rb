1caac20dfa9bff1c5f5989c4b2d20c7909bba567
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-06 07:43:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,8 +47,8 @@ def Song.create_from_filename(name)
     @@all << song
 end 
  
-def Song.new_from_filename
-song = self.new 
+def Song.new_from_filename(name)
+  song = self.new 
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0]) 
     song 
